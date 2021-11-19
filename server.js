c40f8647e654a22362a1e341b92159b08ebe0d83
Christian Hernandez <69328589+DevKhris@users.noreply.github.com>
2021-11-18 21:02:38 -0400

    added http verbs routes

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ const app = express()
 const port = process.env.PORT
 const host = process.env.HOST
 
+var contacts = [
+    {
+        name: 'John Doe',
+        email: 'john@example.com'
+    },
+
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com'
+    }
+]
+
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.set('port', port || 3000)
@@ -18,6 +30,33 @@ app.set('port', port || 3000)
 app.get('/', (req, res) => {
     res.send('Hello API')
 })
+
+app.get('/contacts', (req, res) => {
+    res.send(contacts)
+})
+
+app.get('/contacts/:contactId', (req, res) => {
+    let id = req.params.contactId
+    res.send(contacts[id])
+})
+
+app.post('/contacts', (req, res) => {
+    contacts.push({ name: req.body.name, email: req.body.email })
+    res.send(contacts)
+})
+
+app.put('/contacts/:contactId', (req, res) => {
+    let id = req.params.contactId
+    contacts[id] = { name: req.body.name, email: req.body.email }
+    res.send(contacts[id])
+})
+
+app.delete('/contacts/:contactId', (req, res) => {
+    let id = req.params.contactId
+    contacts.splice(id, 1)
+    res.send(contacts)
+})
+
 
 app.listen(port, host, () => {
     console.info(`[+] Starting server at http://${host}:${port}`)
